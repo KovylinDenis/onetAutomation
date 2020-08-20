263655c9e1c262059d8862d86560bb3954e6ca47
Denys <denys.kovylin@temy.co>
2020-08-20 16:20:07 +0300

    Add auto-retry logic

--- src/model/turnOffFirewall.js
+++ src/model/turnOffFirewall.js
@@ -4,45 +4,49 @@ const puppeteer = require('puppeteer')
 const {readFileAsync} = require(path.resolve('src', 'model', 'utils'))
 const {LoginPage, SettingsPage} = require(path.resolve('src', 'pages'))
 const DATABASE_FILE = path.resolve('src', 'database', 'database.json')
+const retries = 2
 
 const main = async () => {
-  const database = JSON.parse(await readFileAsync(DATABASE_FILE))
+  for (let retry = 0; retry < retries; retry++) {
+    const database = JSON.parse(await readFileAsync(DATABASE_FILE))
 
-  for (let i = 0; i < database.length; i++) {
-    if (!('firewall' in database[i]) || database[i].firewall !== 'off') {
-      console.log('Disabling firewall for:' + database[i].email)
+    for (let i = 0; i < database.length; i++) {
+      if (!('firewall' in database[i]) || database[i].firewall !== 'off') {
+        console.log(
+          `i: ${i} retry: ${retry} Disabling firewall for:' ${database[i].email}`
+        )
 
-      const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        timeout: 30000
-      })
+        const browser = await puppeteer.launch({
+          headless: false,
+          defaultViewport: null,
+          timeout: 30000
+        })
 
-      try {
-        const page = await browser.newPage()
-        const loginPage = new LoginPage({page})
-        const settingsPage = new SettingsPage({page})
+        try {
+          const page = await browser.newPage()
+          const loginPage = new LoginPage({page})
+          const settingsPage = new SettingsPage({page})
 
-        await loginPage.doLogin({credentials: database[i]})
-        if (database[i].imap !== 'on') {
-          await settingsPage.enableIMAP()
-          database[i].imap = 'on'
+          await loginPage.doLogin({credentials: database[i]})
+          if (database[i].imap !== 'on') {
+            await settingsPage.enableIMAP()
+            database[i].imap = 'on'
+          }
+
+          await settingsPage.disableFirewall()
+          database[i].firewall = 'off'
+        } catch (error) {
+          console.log(`ERROR! User: ${database[i].email}\n${error}`)
+          database[i].firewall = 'error: ' + error
+        } finally {
+          await browser.close()
         }
-
-        await settingsPage.disableFirewall()
-        database[i].firewall = 'off'
-      } catch (error) {
-        console.log(`ERROR! User: ${database[i].email}\n${error}`)
-        database[i].firewall = 'error: ' + error
-      } finally {
-        await browser.close()
+      } else {
+        console.log('Firewall is already off for:' + database[i].email)
       }
-    } else {
-      console.log('Firewall is already off for:' + database[i].email)
     }
+    fs.writeFileSync(DATABASE_FILE, JSON.stringify(database, null, 2), () => {})
   }
-
-  fs.writeFileSync(DATABASE_FILE, JSON.stringify(database, null, 2), () => {})
   console.log('Done!')
 }
 
