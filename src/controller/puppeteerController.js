9880f0ce6866a5603f4d07d559295f4264815384
Denys <denys.kovylin@temy.co>
2020-08-21 18:07:32 +0300

    Stabilize + logs

--- src/controller/puppeteerController.js
+++ src/controller/puppeteerController.js
@@ -3,15 +3,13 @@ const puppeteer = require('puppeteer')
 const {LoginPage, SettingsPage} = require(path.resolve('src', 'pages'))
 
 const disableFirewallForUser = async ({user}) => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-    headless: false,
-    executablePath: process.env.PUPPETEER_EXEC_PATH,
-    defaultViewport: null,
-    timeout: 30000
-  })
-
   try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--window-size=1920,1080'],
+      headless: false,
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      timeout: 30000
+    })
     const page = await browser.newPage()
     const loginPage = new LoginPage({page})
     const settingsPage = new SettingsPage({page})
@@ -19,10 +17,14 @@ const disableFirewallForUser = async ({user}) => {
     await loginPage.doLogin({credentials: user})
     if (user.imap !== 'on') {
       await settingsPage.enableIMAP()
+      console.log('   * IMAP enabled')
       user.imap = 'on'
+    } else {
+      console.log('   * IMAP already enabled')
     }
 
     await settingsPage.disableFirewall()
+    console.log('   * Firewall disabled')
     user.firewall = 'off'
   } catch (error) {
     console.log(`ERROR! User: ${user.email}\n${error}`)
--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -31,13 +31,18 @@ module.exports = class SettingsPage {
 
   async disableFirewall() {
     await this.page.goto('https://poczta.onet.pl/ustawienia/')
+    await this.page.waitFor(this.firewallSwitchCss)
+  
     const status = await this.getAttribute(this.firewallSwitchCss, 'class')
 
     if (status === this.switchStateOn) {
+      await this.page.waitFor(this.firewallSwitchCss)
       await this.click(this.firewallSwitchCss)
     }
 
     await this.page.reload()
+    await this.page.waitFor(this.firewallSwitchCss)
+
     const newStatus = await this.getAttribute(this.firewallSwitchCss, 'class')
     if (newStatus === this.switchStateOn) {
       throw new Error('Firewall disabling failed!')
@@ -46,13 +51,18 @@ module.exports = class SettingsPage {
 
   async enableIMAP() {
     await this.page.goto('https://poczta.onet.pl/ustawienia/')
+    await this.page.waitFor(this.IMAPSwitchCss)
+  
     const status = await this.getAttribute(this.IMAPSwitchCss, 'class')
 
     if (status === this.switchStateOff) {
+      await this.page.waitFor(this.IMAPSwitchCss)
       await this.click(this.IMAPSwitchCss)
     }
 
     await this.page.reload()
+    await this.page.waitFor(this.IMAPSwitchCss)
+
     const newStatus = await this.getAttribute(this.IMAPSwitchCss, 'class')
     if (newStatus === this.switchStateOff) {
       throw new Error('IMAP enabling failed!')
