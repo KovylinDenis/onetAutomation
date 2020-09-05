ea98eef65116fecd2ca8502cc8a76cb034fc4a56
Denys <denys.kovylin@temy.co>
2020-09-05 23:48:13 +0300

    Script fix

--- src/sh/commitProcess.sh
+++ src/sh/commitProcess.sh
@@ -7,8 +7,16 @@ then
   echo "Nothing to commit or push after processing"; 
   exit 0; 
 else       
-  echo "'$status'"
-  echo "'$diff'"
-  echo "'$sum'"
+  echo "status: '$status'"
+  echo "diff: '$diff'"
+  echo "sum: '$sum'"
   echo "Commit $i retry"
+  sudo chown -R "${USER:-$(id -un)}" .
+  git config --global user.name "github-actions[bot]";
+  git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"; 
+  git pull origin; 
+  git add -A; 
+  git commit -m "Processed accounts Part $DATABASE_PART"; 
+  git push origin "$GITHUB_SHA-branch";
+  sleep 3
 fi
