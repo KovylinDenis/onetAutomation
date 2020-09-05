8e43c69ceab1ce9fca542b0f4cce9d126c38989e
Denys <denys.kovylin@temy.co>
2020-09-05 21:24:42 +0300

    Check logs

--- src/sh/commitProcess.sh
+++ src/sh/commitProcess.sh
@@ -7,11 +7,15 @@ then
 else 
   git config --global user.name "github-actions[bot]";
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"; 
+  echo "Config stage passed"; 
   git pull origin; 
+  echo "Pulled"; 
   #git stash; 
   #git checkout --track origin/"$GITHUB_SHA-branch"; 
   #git stash pop; 
   git add -A; 
+  echo "staged"; 
   git commit -m "Processed accounts Part $DATABASE_PART"; 
+  echo "commited"; 
   git push origin "$GITHUB_SHA-branch";
 fi
