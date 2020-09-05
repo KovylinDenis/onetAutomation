b193dc33743121f45f5c8b8343a914391d29d7d7
Denys <denys.kovylin@temy.co>
2020-09-05 23:45:30 +0300

    as

--- src/sh/commitProcess.sh
+++ src/sh/commitProcess.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 status="$(git status --short)"
 diff="$(git diff master origin/master)"
-sum=$status+$diff
+sum=$status$diff
 if [ -z "$sum" ]
 then 
-  echo "Nothing to commit after processing"; 
+  echo "Nothing to commit or push after processing"; 
   exit 0; 
 else       
-  echo $status
-  echo $diff
+  echo "'$status'"
+  echo "'$diff'"
+  echo "'$sum'"
   echo "Commit $i retry"
 fi
