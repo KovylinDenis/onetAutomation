4918461edc83596fab4897de1ff5e6f2ccc36589
Denys <denys.kovylin@temy.co>
2020-09-05 22:46:37 +0300

    Use implement retry logic on nodejs level

---
+++ src/model/loopCommit.js
@@ -0,0 +1,25 @@
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+const commitRetries = 6
+
+const sleep = async (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const main = async () => {
+  for (let i = 0; i < commitRetries; i++) {
+    try {
+      const { stdout, stderr } = await exec('sh ./src/sh/commitProcess.sh')
+      console.log('stdout:', stdout)
+      console.log('stderr:', stderr)
+    } catch (err) {
+      console.error(err)
+    }
+    await sleep(1000 + Math.floor(Math.random() * Math.floor(5000)))
+  }
+}
+
+;(async () => {
+  await main()
+})()
