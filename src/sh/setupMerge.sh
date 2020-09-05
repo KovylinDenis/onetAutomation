561a0737c0172a506d37648cf660c89502314056
Denys <denys.kovylin@temy.co>
2020-09-05 20:39:14 +0300

    Add scripts for pre-checkout

---
+++ src/sh/setupMerge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "github-actions[bot]";
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"; 
+git pull origin;
+git checkout --track origin/"$GITHUB_SHA-branch";
+npm run mergeAccounts
