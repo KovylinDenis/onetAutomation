7285d9389fbf4272759f5400abd7c4573a89451c
Denys <denys.kovylin@temy.co>
2020-09-05 23:42:09 +0300

    test

--- src/sh/commitProcess.sh
+++ src/sh/commitProcess.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
-if [[ (-z "$(git status --short)"]) || ( -z "$(git diff $GITHUB_SHA-branch origin/$GITHUB_SHA-branch)")]]
+status="$(git status --short)"
+diff="$(git diff master origin/master)"
+sum=$status+$diff
+if [ -z "$sum" ]
 then 
   echo "Nothing to commit after processing"; 
   exit 0; 
 else       
+  echo $status
+  echo $diff
   echo "Commit $i retry"
-  sudo chown -R "${USER:-$(id -un)}" .
-  git config --global user.name "github-actions[bot]";
-  git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"; 
-  git pull origin; 
-  git add -A; 
-  git commit -m "Processed accounts Part $DATABASE_PART"; 
-  git push origin "$GITHUB_SHA-branch";
-  sleep 3
 fi
