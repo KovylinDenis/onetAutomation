5d572440f59393542e72bec13b226a8f8dd7e645
Denys <denys.kovylin@temy.co>
2020-09-06 00:21:33 +0300

    Update commands & swap pull

--- src/sh/commitProcess.sh
+++ src/sh/commitProcess.sh
@@ -14,9 +14,11 @@ else
   sudo chown -R "${USER:-$(id -un)}" .
   git config --global user.name "github-actions[bot]";
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"; 
-  git pull origin; 
+  git config --global pull.rebase true
+  git config --global rebase.autoStash true
   git add -A; 
   git commit -m "Processed accounts Part $DATABASE_PART"; 
+  git pull --rebase origin "$GITHUB_SHA-branch"; 
   git push --force origin "$GITHUB_SHA-branch";
   sleep 3
 fi
