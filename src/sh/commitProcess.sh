8d837f0f270a4a9bdcb6e40fb24e2698786ad8e2
Denys <denys.kovylin@temy.co>
2020-09-05 21:34:13 +0300

    check if chown helps

--- src/sh/commitProcess.sh
+++ src/sh/commitProcess.sh
@@ -5,17 +5,11 @@ then
   echo "Nothing to commit after processing"; 
   exit 0; 
 else 
+  sudo chown -Rc $UID .git/
   git config --global user.name "github-actions[bot]";
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"; 
-  echo "Config stage passed"; 
   git pull origin; 
-  echo "Pulled"; 
-  #git stash; 
-  #git checkout --track origin/"$GITHUB_SHA-branch"; 
-  #git stash pop; 
   git add -A; 
-  echo "staged"; 
   git commit -m "Processed accounts Part $DATABASE_PART"; 
-  echo "commited"; 
   git push origin "$GITHUB_SHA-branch";
 fi
