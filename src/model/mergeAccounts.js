006e2274b75497088c57a54ce2adc95b195c8a60
Denys <denys.kovylin@temy.co>
2020-09-06 10:14:12 +0300

    Fix artifact folder path + 16 more accounts to process

--- src/model/mergeAccounts.js
+++ src/model/mergeAccounts.js
@@ -5,14 +5,21 @@ const {readFileAsync} = require(path.resolve('src', 'model', 'utils'))
 const PARTS_COUNT = 15
 const DATABASE_FILE = path.resolve('src', 'database', 'database.json')
 const DATABASE_TEMP_FOLDER = path.resolve('src', 'database', 'temp')
+const ARTIFACTS_FOLDER = path.resolve('src', 'database', 'temp', 'artifacts')
 
 const main = async () => {
   const mainDatabase = JSON.parse(await readFileAsync(DATABASE_FILE))
 
   for (let i = 0; i < PARTS_COUNT; i++) {
     const partDatabasePath = path.resolve(DATABASE_TEMP_FOLDER, `${i}.json`)
-    const artifactDatabasePath = path.resolve(DATABASE_TEMP_FOLDER,`` , `${i}.json`)
-    const partArtifactDatabase = JSON.parse(await readFileAsync(artifactDatabasePath))
+    const artifactDatabasePath = path.resolve(
+      ARTIFACTS_FOLDER,
+      `temp-artifact-processed-${i}`,
+      `${i}.json`
+    )
+    const partArtifactDatabase = JSON.parse(
+      await readFileAsync(artifactDatabasePath)
+    )
 
     for (let j = 0; j < partArtifactDatabase.length; j++) {
       const index = mainDatabase.findIndex(
