7c0bbc05bc06571529f241c6f1252b03a043f327
Denys <denys.kovylin@temy.co>
2020-09-06 09:56:06 +0300

    Adjust merge script to match artifacts process

--- src/model/mergeAccounts.js
+++ src/model/mergeAccounts.js
@@ -11,15 +11,16 @@ const main = async () => {
 
   for (let i = 0; i < PARTS_COUNT; i++) {
     const partDatabasePath = path.resolve(DATABASE_TEMP_FOLDER, `${i}.json`)
-    const partDatabase = JSON.parse(await readFileAsync(partDatabasePath))
+    const artifactDatabasePath = path.resolve(DATABASE_TEMP_FOLDER,`` , `${i}.json`)
+    const partArtifactDatabase = JSON.parse(await readFileAsync(artifactDatabasePath))
 
-    for (let j = 0; j < partDatabase.length; j++) {
+    for (let j = 0; j < partArtifactDatabase.length; j++) {
       const index = mainDatabase.findIndex(
-        (element) => element.email === partDatabase[j].email
+        (element) => element.email === partArtifactDatabase[j].email
       )
       console.log(`Part: ${i}. PartUser ${j}`)
-      console.log(partDatabase)
-      mainDatabase[index] = partDatabase[j]
+      console.log(partArtifactDatabase)
+      mainDatabase[index] = partArtifactDatabase[j]
     }
 
     await fs.writeFile(partDatabasePath, JSON.stringify([], null, 2))
